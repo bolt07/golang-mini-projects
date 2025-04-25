6e6146119a8560fac923828066fa747ad0a7d480
bold07 <itshiteshkumar3816@gmail.com>
2025-04-25 11:03:19 +0530

    updating handler functions

--- jwt/main.go
+++ jwt/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
+	"net/http"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
@@ -79,4 +81,76 @@ func ValidateJWT(tokenString string) bool {
 
 	log.Println("invalid token or claims")
 	return false
+}
+
+// middleware authentication
+func Auth(handler http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		token := r.Header.Get("Token")
+		if token == "" {
+			http.Error(w, "Missing token", http.StatusUnauthorized)
+			return
+		}
+
+		if !ValidateJWT(token) {
+			http.Error(w, "Invalid or expired token", http.StatusUnauthorized)
+			return
+		}
+
+		handler(w, r)
+	}
+}
+
+func LoginHanlder(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var userData User
+
+	if err := json.NewDecoder(r.Body).Decode(&userData); err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+	defer r.Body.Close()
+
+	log.Printf("Login attempt - User name: %s and Password: %s\n", userData.UserName, userData.Password)
+
+	if userData.UserName != "admin" || userData.Password != "admin" {
+		http.Error(w, "Invalid credentials", http.StatusUnauthorized)
+		return
+	}
+
+	token, err := CreateJWT()
+	if err != nil {
+		log.Printf("Error creating token: %v", err)
+		http.Error(w, "Faild to generate token", http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(map[string]string{
+		"token": token,
+	})
+}
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write(jsonMessageBytes("Success", "Welcome to Golang with JWT authentication"))
+}
+
+func SecureHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write(jsonMessageBytes("Success", "Correct JWT token"))
+}
+
+func main() {
+	fmt.Println("JWT - authentication with Golang")
+
+	http.HandleFunc("/", HomeHandler)
+
+	http.HandleFunc("/login", LoginHanlder)
+
+	http.Handle("/secure", Auth(SecureHandler))
+
+	http.ListenAndServe(":8080", nil)
 }
